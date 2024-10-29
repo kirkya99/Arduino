149934ef450106707e826bac3134730d6d18937a
kirkya99 <fabianschotte@outlook.com>
2024-10-29 14:26:37 +0100

    update 2

---
+++ Embedded Systems/TASTEN_LED/TASTEN_LED.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(3) == HIGH) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+}
